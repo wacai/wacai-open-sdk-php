deaf6d1b6cc5d4daba1a56b20318828cf0c6c9d0
shaofeng <guoqing.liu@husor.com>
2018-04-10 22:38:50 +0800

    修改read me

--- src/api_post_demo.php
+++ src/api_post_demo.php
@@ -4,11 +4,12 @@ require_once './client_api.php';
 $api_name = "api.test.post.fixed";
 // 调用的API版本
 $api_version = "1.0";
-// Api服务
+// Api服务初始化
 $client_api = new ClientApi($api_name, $api_version);
-// 业务参数(json)--demo
+// 业务参数(json格式)
 $body_data = '{"uid":123,"name":"zy"}';
-
+// Api调用
 $client_api->http_post($body_data, false, $res);
+// 查看调动结果
 var_dump($res);
 ?>
